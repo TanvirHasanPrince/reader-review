6e07f9256cd9a2d19013090ffa9b8a4b3bf40b5d
unknown <t.hasan.prince@gmail.com>
2023-07-31 16:24:16 +0600

    [Done]- Responsive Book Card

--- src/components/cards/BookCard.tsx
+++ src/components/cards/BookCard.tsx
@@ -23,11 +23,11 @@ const BookCard = ({ book }: BookCardProps) => {
           />
         </div>
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-        <div className="absolute inset-0 flex translate-y-[80%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <div className="absolute inset-0 flex lg:translate-y-[80%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 className="font-dmserif text-3xl font-bold text-white">
             {title}
           </h1>
-          <p className="mb-3 md:text-lg lg:text-lg text-sm italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-ellipsis">
+          <p className="mb-3 md:text-lg lg:text-lg text-sm italic text-white font-bold lg:opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-ellipsis">
             {author}
           </p>
         </div>
