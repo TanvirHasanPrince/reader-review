72dc1ff3ead087f15ce0586b1e16539c6702adba
unknown <t.hasan.prince@gmail.com>
2023-08-01 14:24:10 +0600

    [Done]-Seach and Filter

--- src/pages/allBooks/AllBooks.tsx
+++ src/pages/allBooks/AllBooks.tsx
@@ -6,50 +6,96 @@ import { useGetbooksQuery } from "../../redux/features/books/booksApi";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import {
   updateQuery,
+  updateGenreFilter,
+  updatePublicationYearFilter,
   updateResults,
 } from "../../redux/features/books/bookSlice";
 
 const AllBooks = () => {
   const dispatch = useAppDispatch();
   const { data, isError } = useGetbooksQuery(undefined);
-  const { query, results } = useAppSelector((state) => state.book);
+  const { query, genreFilter, publicationYearFilter, results } = useAppSelector(
+    (state) => state.book
+  );
 
   useEffect(() => {
-    // When the query changes, fetch the data and update the results
+    // When the query or filters change, fetch the data and update the results
+    let filteredBooks = data?.data || [];
     if (query) {
-      const filteredBooks = data?.data.filter(
+      filteredBooks = filteredBooks.filter(
         (book: IBook) =>
           book.title.toLowerCase().includes(query.toLowerCase()) ||
           book.author.toLowerCase().includes(query.toLowerCase())
       );
-      dispatch(updateResults(filteredBooks || []));
-    } else {
-      // If the query is empty, reset the results to the full data
-      dispatch(updateResults(data?.data || []));
     }
-  }, [query, data, dispatch]);
+    if (genreFilter) {
+      filteredBooks = filteredBooks.filter((book: IBook) =>
+        book.genre.toLowerCase().includes(genreFilter.toLowerCase())
+      );
+    }
+    if (publicationYearFilter) {
+      filteredBooks = filteredBooks.filter((book: IBook) =>
+        book.publication_data.includes(publicationYearFilter)
+      );
+    }
+    dispatch(updateResults(filteredBooks));
+  }, [query, genreFilter, publicationYearFilter, data, dispatch]);
 
   if (isError) {
     return <div>Error occurred while fetching data.</div>;
   }
 
-  const booksToShow = query ? results : data?.data || [];
+  const genres = Array.from(
+    new Set(data?.data.map((book: IBook) => book.genre)) || []
+  );
+  const publicationYears = Array.from(
+    new Set(data?.data.map((book: IBook) => book.publication_data)) || []
+  );
+
+  const booksToShow =
+    query || genreFilter || publicationYearFilter ? results : data?.data || [];
 
   return (
-    <div className="flex">
-      <div className="m-10 flex ">
+    <div className="lg:flex m-5 lg:m-2 md:m-5">
+      <div className="m-10 flex flex-col">
         <input
           type="text"
           placeholder="Search here"
-          className="input input-bordered"
+          className="input input-bordered w-full"
           onChange={(e) => dispatch(updateQuery(e.target.value))}
         />
-        <button
-          className="btn btn-secondary px-5 ml-5 "
-          onClick={() => dispatch(updateQuery(""))}
-        >
-          Search
-        </button>
+        <div className="py-2">
+          <select
+            className="select select-bordered w-full"
+            value={genreFilter || ""}
+            onChange={(e) =>
+              dispatch(updateGenreFilter(e.target.value || null))
+            }
+          >
+            <option value="">Filter by Genre</option>
+            {genres.map((genre) => (
+              <option key={genre} value={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          <select
+            className="select select-bordered w-full"
+            value={publicationYearFilter || ""}
+            onChange={(e) =>
+              dispatch(updatePublicationYearFilter(e.target.value || null))
+            }
+          >
+            <option value="">Filter by Publication Year</option>
+            {publicationYears.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className="grid grid-cols-1 gap-5 md:grid-cols-1 lg:grid-cols-4">
         {booksToShow.map((book: IBook) => (
--- src/redux/features/books/bookSlice.ts
+++ src/redux/features/books/bookSlice.ts
@@ -4,11 +4,15 @@ import { IBook } from "../../../components/cards/BookCard";
 
 interface SearchState {
   query: string;
+  genreFilter: string | null;
+  publicationYearFilter: string | null;
   results: IBook[];
 }
 
 const initialState: SearchState = {
   query: "",
+  genreFilter: null,
+  publicationYearFilter: null,
   results: [],
 };
 
@@ -19,11 +23,25 @@ const bookSlice = createSlice({
     updateQuery: (state, action: PayloadAction<string>) => {
       state.query = action.payload;
     },
+    updateGenreFilter: (state, action: PayloadAction<string | null>) => {
+      state.genreFilter = action.payload;
+    },
+    updatePublicationYearFilter: (
+      state,
+      action: PayloadAction<string | null>
+    ) => {
+      state.publicationYearFilter = action.payload;
+    },
     updateResults: (state, action: PayloadAction<IBook[]>) => {
       state.results = action.payload;
     },
   },
 });
 
-export const { updateQuery, updateResults } = bookSlice.actions;
+export const {
+  updateQuery,
+  updateResults,
+  updateGenreFilter,
+  updatePublicationYearFilter,
+} = bookSlice.actions;
 export default bookSlice.reducer;
